7a50b63f28129710fe7378edb4703c749da6c1cc
Nandinivijakumar <csmnandhini@gmail.com>
2017-04-06 09:39:19 +0530

    Demo_new1 checked in

---
+++ Demo_new1/src/main/java/com/orion/DemoNew1Application.java
@@ -0,0 +1,12 @@
+package com.orion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoNew1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoNew1Application.class, args);
+	}
+}
